9ea3d9733f7691116bc9a5a9eeed2304f9759649
didemtemel <44496208+didemtemel@users.noreply.github.com>
2020-05-04 11:25:32 +0300

    last but not least

---
+++ CetBookStore/ViewComponents/SearchCategoryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using CetBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewComponents
+{
+    public class SearchCategoryViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public SearchCategoryViewComponent(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await dbContext.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
